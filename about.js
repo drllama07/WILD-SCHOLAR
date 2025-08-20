5ae79e175f4e89795a9281b69b3bb233eec6781c
Yusuf Yilmaz <yusufyilmaz@Yusufs-Mac-mini.local>
2025-08-20 19:53:25 +0300

    ssdf

--- about.js
+++ about.js
@@ -1,19 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const intro = document.querySelector(".intro");
 
-    function checkScroll() {
-        let scrollY = window.scrollY;
-
-        // Hide intro text when scrolling down
-        if (scrollY > 50) {
-            intro.style.opacity = "0";
-        } else {
-            intro.style.opacity = "1";
-        }
-    }
-
-    window.addEventListener("scroll", checkScroll);
-});
 
 
 
